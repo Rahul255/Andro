12171468033da08fdc1c0b1873474db522f6bd1a
Rahul255 <rahulmohan255@gmail.com>
2019-06-11 16:12:16 +0530

    file added

--- UserInput2/app/src/main/java/com/example/userinput2/MainActivity.java
+++ UserInput2/app/src/main/java/com/example/userinput2/MainActivity.java
@@ -20,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
         display1("This is Box 1.");
         display2("And this is Box 2.");
         display3("And look! Box 3!");
+        int weekday = 5;
+        int weekend = 9;
+        int optimalHours = 7 * 8;
+
+        int actualHours = weekday;
+        actualHours = actualHours + weekend * 2;
+        int solution = optimalHours - actualHours;
+        display(solution);
 
     }
 

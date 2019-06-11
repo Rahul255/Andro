6be71083db465e83e4677035cce57de0a88b6a11
Rahul255 <rahulmohan255@gmail.com>
2019-06-11 12:25:48 +0530

    file added

--- UserInput2/app/src/main/java/com/example/userinput2/MainActivity.java
+++ UserInput2/app/src/main/java/com/example/userinput2/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.userinput2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // PASTE CODE YOU WANT TO TEST
+
+        int raspberryPrice = 5;
+        display1("1 box: $" + raspberryPrice);
+        raspberryPrice = 10;
+        display2("2 boxes: $" + (raspberryPrice));
+        display3("3 boxes: $" + (raspberryPrice * 3));
+        display1("This is Box 1.");
+        display2("And this is Box 2.");
+        display3("And look! Box 3!");
+
+    }
+
+    /**
+     * Display methods that allow the text to appear on the screen. Don't worry if you don't know
+     * how these work yet. We'll be covering them in lesson 3.
+     */
+
+    public void display(String text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view);
+        t.setText(text);
+    }
+
+    public void display(int text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view);
+        t.setText(text + "");
+    }
+
+    public void display1(String text) {
+        display(text);
+    }
+
+    public void display2(String text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view_2);
+        t.setText(text);
+    }
+
+    public void display3(String text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view_3);
+        t.setText(text);
     }
 }

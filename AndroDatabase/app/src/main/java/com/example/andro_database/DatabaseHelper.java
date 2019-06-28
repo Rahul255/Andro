587e2bae19a169a46b86095b6be584e31c3f334f
Rahul255 <rahulmohan255@gmail.com>
2019-06-28 10:33:47 +0530

    file added

---
+++ AndroDatabase/app/src/main/java/com/example/andro_database/DatabaseHelper.java
@@ -0,0 +1,21 @@
+package com.example.andro_database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public DatabaseHelper(@androidx.annotation.Nullable Context context, @androidx.annotation.Nullable String name, @androidx.annotation.Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}

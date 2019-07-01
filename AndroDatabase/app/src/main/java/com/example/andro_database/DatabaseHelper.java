66b70cad994dc17a04d92eb80464205f1292af37
Rahul255 <rahulmohan255@gmail.com>
2019-07-01 15:21:41 +0530

    file added

--- AndroDatabase/app/src/main/java/com/example/andro_database/DatabaseHelper.java
+++ AndroDatabase/app/src/main/java/com/example/andro_database/DatabaseHelper.java
@@ -2,6 +2,7 @@ package com.example.andro_database;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -44,6 +45,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         else
             return true;
 
+
+    }
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from "+TABLE_NAME,null);
+        return res;
 
     }
 }
--- AndroDatabase/app/src/main/java/com/example/andro_database/MainActivity.java
+++ AndroDatabase/app/src/main/java/com/example/andro_database/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.andro_database;
 
+import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     DatabaseHelper myDb;
     EditText editName,editSurname,editMarks;
     Button btnAddData;
+    Button btnviewAll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +26,9 @@ public class MainActivity extends AppCompatActivity {
         editSurname=findViewById(R.id.editText2);
         editMarks=findViewById(R.id.editText3);
         btnAddData=findViewById(R.id.button_add);
+        btnviewAll=findViewById(R.id.button_view);
         AddData();
+        viewAll();
     }
     public void AddData() {
         btnAddData.setOnClickListener(
@@ -40,5 +45,38 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+    }
+    public void viewAll() {
+        btnviewAll.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Cursor res = myDb.getAllData();
+                        if(res.getCount() == 0){
+                            //show msg
+                            showMessage( "Error", "Nothing found");
+                            return;
+                        }
+                        StringBuffer buffer =new StringBuffer();
+                        while (res.moveToNext()){
+                            buffer.append("ID :"+ res.getString(0)+"\n");
+                            buffer.append("Name :"+ res.getString(1)+"\n");
+                            buffer.append("Surname :"+ res.getString(2)+"\n");
+                            buffer.append("Marks :"+ res.getString(3)+"\n\n");
+                        }
+                        //show all data
+                        showMessage("Data",buffer.toString());
+
+
+                    }
+                }
+        );
+    }
+    public void showMessage(String title, String Message){
+        AlertDialog.Builder builder= new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
     }
 }

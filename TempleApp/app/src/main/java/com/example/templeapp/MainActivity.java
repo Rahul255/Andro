747f450b7ee3dbad2713e671669fbd5a3f8b4fae
Rahul255 <rahulmohan255@gmail.com>
2019-07-03 11:58:13 +0530

    file added

--- TempleApp/app/src/main/java/com/example/templeapp/MainActivity.java
+++ TempleApp/app/src/main/java/com/example/templeapp/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.templeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button newAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        newAccount=findViewById(R.id.newAccount);
+        newAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAccount();
+            }
+        });
+    }
+    public void openAccount(){
+        Intent intent= new Intent(this, newAccount.class);
+        startActivity(intent);
     }
 }
+
---
+++ TempleApp/app/src/main/java/com/example/templeapp/newAccount.java
@@ -0,0 +1,22 @@
+package com.example.templeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class newAccount extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_account);
+    }
+
+
+
+    }
+

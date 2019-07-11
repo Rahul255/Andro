ba3c1215d2a330c4f5b4833808511a077eef10c4
Rahul255 <rahulmohan255@gmail.com>
2019-07-11 08:37:37 +0530

    file added

--- LoginApplication/app/src/main/java/com/example/loginapplication/MainActivity.java
+++ LoginApplication/app/src/main/java/com/example/loginapplication/MainActivity.java
@@ -8,12 +8,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText mTextUsername;
     EditText mTextPassword;
     Button mButtonLogin;
     TextView mTextViewRegister;
+    DatabaseHelper db;
 
 
     @Override
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        db = new DatabaseHelper(this);
         mTextUsername=(EditText) findViewById(R.id.editTextUsername);
         mTextPassword=(EditText) findViewById(R.id.editTextPassword);
         mButtonLogin=(Button) findViewById(R.id.button_login);
@@ -30,6 +33,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 openActivity();
+            }
+        });
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = mTextUsername.getText().toString().trim();
+                String pwd = mTextPassword.getText().toString().trim();
+                Boolean res = db.checkUser(user,pwd);
+                if(res == true){
+                    Toast.makeText( MainActivity.this,"Successfully Logged In",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText( MainActivity.this,"Login Error",Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
--- LoginApplication/app/src/main/java/com/example/loginapplication/RegisterActivity.java
+++ LoginApplication/app/src/main/java/com/example/loginapplication/RegisterActivity.java
@@ -8,8 +8,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+    DatabaseHelper db;
     EditText mTextUsername;
     EditText mTextPassword;
     EditText mTextCnfPassword;
@@ -21,6 +23,7 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        db = new DatabaseHelper(this);
         mTextUsername=findViewById(R.id.editTextUsername);
         mTextPassword=findViewById(R.id.editTextPassword);
         mTextCnfPassword=findViewById(R.id.editTextCnfPassword);
@@ -31,6 +34,31 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 openActivity();
+            }
+        });
+
+        mButtonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user= mTextUsername.getText().toString().trim();
+                String pwd= mTextPassword.getText().toString().trim();
+                String cnf_pwd= mTextCnfPassword.getText().toString().trim();
+
+                if(pwd.equals(cnf_pwd)){
+                    long val = db.addUser(user,pwd);
+                    if(val > 0){
+                        Toast.makeText( RegisterActivity.this,"You have Registered",Toast.LENGTH_SHORT).show();
+                        Intent moveToLogin = new Intent(RegisterActivity.this,MainActivity.class);
+                        startActivity(moveToLogin);
+                    }
+                    else {
+                        Toast.makeText( RegisterActivity.this,"Registration Error",Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                else{
+                    Toast.makeText( RegisterActivity.this,"Password is not matching",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
